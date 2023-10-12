c526de23e731b5b943ae28c1d31689d5b2ac5e82
Raffs <ajrafferty67@gmail.com>
2023-10-12 15:58:44 +1100

    1st postgress flexible server upload to github

---
+++ secrets.tf
@@ -0,0 +1,20 @@
+variable "ARM_CLIENT_ID" {
+  type    = string
+  default = "5c52a243-67af-4b46-a2fd-c451de6c0f40"
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type    = string
+  default = "c_k8Q~0V4g06MEY6Q1Uua_EPgEBwg4D0Km10edtV"
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  type    = string
+  default = "cfbfc6c9-d0ee-4d3d-93a2-5bfaaf1a8be1"
+}
+
+variable "ARM_TENANT_ID" {
+  type    = string
+  default = "26934fe8-ff63-4a4f-8f21-94a39b55dc19"
+}
+
